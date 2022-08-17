2de706ca4ed4579411963750853d8e958159e552
megamind <echingalo@gmail.com>
2022-08-17 18:50:03 +0300

    feat:

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,6 @@ import classes from "./App.module.css";
 import CustomTableContainer from "./components/CustomTableContainer";
 import DataSelectionContainer from "./components/DataSelectionContainer";
 
-const MyApp = () => (
-  <div className={classes.container}>
-    <DataSelectionContainer
-      onChangeSelection={() => console.log("selection changed")}
-    />
-    <CustomTableContainer />
-  </div>
-);
+const MyApp = () => <div className={classes.container}></div>;
 
 export default MyApp;
